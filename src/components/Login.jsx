528d0ed681f93925fe38dbbcb0382cb52e2b2f02
aartibisht123 <aartbisht@gmail.com>
2025-12-21 14:39:46 +0530

    new file:   src/utils/userSlice.js

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,29 @@
 import axios from 'axios';
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../utils/constants';
 
 const Login = () => {
 
     const [emailId, setEmailId] = useState("aartisingh@gmail.com");
     const [password, setPassword] = useState("Aartisingh@1234");
-
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
     const handleLogin = async () => {
+      
         try {
-          const res =  await axios.post('http://localhost:3000/login',
+          const res =  await axios.post(BASE_URL + '/login',
             {
           emailId,
           password,
-            },
+            }, 
             {withCredentials: true}
           );
+         
+          dispatch(addUser(res.data))
+         return navigate("/");
         }catch (error) {
             console.log(error);
         }};
---
+++ src/utils/appStore.js
@@ -0,0 +1,11 @@
+import { configureStore, ReducerType } from "@reduxjs/toolkit";
+import userReducer from './userSlice'
+
+const appStore = configureStore({
+    reducer: {
+        user: userReducer,
+    },
+}
+)
+
+export default appStore
