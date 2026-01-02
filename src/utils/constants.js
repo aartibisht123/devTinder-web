cac20fdbe8ea15a8e60717840a7161463a49ba00
aartibisht123 <aartbisht@gmail.com>
2026-01-02 15:37:11 +0530

    modified:   src/utils/constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,8 @@
 // export const BASE_URL = "/api";
 
 //dev
-export const BASE_URL = 
-location.hostname === "localhost" ? "http://localhost:3000" : "/api";
+// export const BASE_URL = "http://localhost:3000";
+
+
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:3000": "/api";
