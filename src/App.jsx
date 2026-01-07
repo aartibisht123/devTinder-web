ca247b615817bfb3a2c617bba12e2545422679f7
aartibisht123 <aartbisht@gmail.com>
2026-01-07 20:10:27 +0530

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import appStore from "./utils/appStore"
 import Feed from "./components/Feed"
 import Connections from "./components/Connections"
 import Requests from "./components/Requests"
-import Premium from "./components/premium"
 import Chat from "./components/Chat"
+import Premium from "./components/premium"
 
 
 function App() {
